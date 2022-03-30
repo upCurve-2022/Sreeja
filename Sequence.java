071f215be6459ad8580a1822fdd07d9cb0c7a043
Sreeja-Y <101651201+Sreeja-Y@users.noreply.github.com>
2022-03-30 22:13:35 +0530

    Add files via upload

---
+++ Sequence.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Sequence
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int x;
+for(int i=1;i<=n;i++)
+{
+x=i*i;
+System.out.print(x );
+}
+}
+}
